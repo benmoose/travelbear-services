0dc5d5f61ccaeb2654ec32179921e2807e735539
Ben Hadfield <benjohnhadfield@gmail.com>
2018-11-17 20:30:33 +0000

    add user

--- travelbear/db_layer/user/models/user.py
+++ travelbear/db_layer/user/models/user.py
@@ -5,6 +5,8 @@ from db_layer.model_base import ModelBase
 
 class User(ModelBase):
     auth0_id = models.CharField(max_length=255, null=False, blank=False, unique=True, db_index=True)
-    name = models.TextField()
-    short_name = models.TextField()
-    picture = models.URLField()
+
+    email = models.EmailField(blank=True)
+    full_name = models.TextField(blank=True)
+    short_name = models.TextField(blank=True)
+    picture = models.URLField(blank=True)
