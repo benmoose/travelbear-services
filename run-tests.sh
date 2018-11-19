4d0cc805248374f7baa6d903472a76a875aedc90
Ben Hadfield <benjohnhadfield@gmail.com>
2018-11-19 22:05:26 +0000

    pass args through to pytest

--- run-tests.sh
+++ run-tests.sh
@@ -2,4 +2,4 @@
 
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml \
     run -e DJANGO_SETTINGS_MODULE=django_conf.settings \
-    server pytest --doctest-modules travelbear
+    server pytest --doctest-modules travelbear $@
