50ab8cbfa2dc8b6962215cf3f708d33c5141a2fa
Ben Hadfield <ben.hadfield@citymapper.com>
2018-11-25 14:14:52 +0000

    add attrs requirement

--- travelbear/api_public/events/views/create_events.py
+++ travelbear/api_public/events/views/create_events.py
@@ -5,5 +5,9 @@ from api_public.auth import require_jwt_auth
 
 @require_http_methods(['POST'])
 @require_jwt_auth
-def create_event():
+def create_event(request):
+    pass
+
+
+def get_event_from_request(request)
     pass
