885eea3a7a7ff97abbae8f8439a8cf0d0420e112
Ben Hadfield <ben.hadfield@citymapper.com>
2018-12-03 19:30:06 +0000

    move deletion method and test

--- travelbear/db_layer/trip/__init__.py
+++ travelbear/db_layer/trip/__init__.py
@@ -4,3 +4,4 @@ from .models.move import Move
 
 from .interface.location_layer import create_location, delete_location
 from .interface.trip_layer import create_trip, delete_trip, list_trips_for_user
+from .interface.move_layer import create_move, delete_move
--- travelbear/db_layer/trip/interface/move_layer.py
+++ travelbear/db_layer/trip/interface/move_layer.py
@@ -1,3 +1,5 @@
+from django.db import transaction
+
 from db_layer.trip import Move
 
 
@@ -9,3 +11,13 @@ def create_move(start_location, end_location, travel_method="", depart_time=None
         depart_time=depart_time,
         arrive_time=arrive_time,
     )
+
+
+def delete_move(move):
+    with transaction.atomic():
+        move = Move.objects.select_for_update().get(pk=move.pk)
+        if move.is_deleted:
+            return move
+        move.is_deleted = True
+        move.save(update_fields=["is_deleted", "modified_on"])
+    return move
