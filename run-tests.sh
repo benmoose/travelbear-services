3d46b75b2978ede1807525923c4ad515ccd455b4
Ben Hadfield <ben.hadfield@citymapper.com>
2019-02-07 19:59:20 +0000

    add common pytest config

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker-compose -f docker-compose.dev.yml -f docker-compose.yml \
-    run --rm -e stack=test server pytest \
-    --doctest-modules --rootdir travelbear --pyargs "$@"
+    run --rm -e stack=test server \
+        pytest --pyargs "$@"
