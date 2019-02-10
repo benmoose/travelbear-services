a8b4c873e134f118928e3f42c5c1aee7c68c9814
Ben Hadfield <ben.hadfield@citymapper.com>
2019-02-10 22:22:31 +0000

    add stub endpoint

--- travelbear/api_public/trips/urls.py
+++ travelbear/api_public/trips/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import create_location, handlers, update_location
+from .views import create_location, handlers, update_location, itinerary
 
 urlpatterns = [
     path("", handlers.root_endpoint),
     path("<trip_id>/", handlers.trip_id_endpoint),
+    path("<trip_id>/itinerary/", itinerary.itinerary_handler),
     path("<trip_id>/locations/", create_location.create_location_handler),
     path("<trip_id>/locations/<location_id>", update_location.update_location_handler),
 ]
