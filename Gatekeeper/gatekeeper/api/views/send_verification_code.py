ebe71f70333dac20bab6ddc48e972859502d91c9
Ben Hadfield <ben.hadfield@citymapper.com>
2019-05-07 21:26:51 +0100

    add phonenumberslite requirement

--- Gatekeeper/gatekeeper/api/views/send_verification_code.py
+++ Gatekeeper/gatekeeper/api/views/send_verification_code.py
@@ -1,10 +1,14 @@
+import re
 from typing import Optional
 
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
+from phonenumbers import parse, format_number, PhoneNumberFormat
 
 from common.model import data_model
 from common.response import error_response, success_response
+
+STRING_LIKELY_MOBILE_NUMBER = re.compile(r"07\d{9}")
 
 
 @data_model
@@ -18,6 +22,10 @@ def send_verification_code(request) -> HttpResponse:
     if request_data is None:
         return error_response()
 
+    phone_number = get_e164_phone_number(request_data.phone_number, "GB")
+    if phone_number is None:
+        return error_response("invalid phone_number")
+
     return success_response({"phone_number": request_data.phone_number})
 
 
@@ -25,4 +33,27 @@ def get_request_data(request_body: dict) -> Optional[RequestData]:
     try:
         return RequestData.from_dict(request_body)
     except TypeError:
+        return None
+
+
+def get_e164_phone_number(phone_number: str, region: str) -> Optional[str]:
+    """
+    >>> get_e164_phone_number("+447000000000", "GB")
+    '+447000000000'
+    >>> get_e164_phone_number("07-000 000 00 0", "GB")
+    '+447000000000'
+    >>> get_e164_phone_number("07000000000", "GB")
+    '+447000000000'
+    >>> get_e164_phone_number("07000000000", "US")
+    '+107000000000'
+    >>> get_e164_phone_number("7000000000", "GB")
+    '+447000000000'
+    >>> get_e164_phone_number("+447a0000000b", "GB")
+    '+4470000000'
+    >>> get_e164_phone_number("", "GB")
+    """
+    try:
+        parsed_phone_number = parse(phone_number, region=region)
+        return format_number(parsed_phone_number, PhoneNumberFormat.E164)
+    except Exception:
         return None
